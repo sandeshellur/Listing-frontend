e9f61a0b6e5d03a874bce294c8a25b1e14463025
sandesh ellur <sandeshellur@gmail.com>
2022-11-17 22:57:22 +0530

    some changes

--- src/app/property/property.component.ts
+++ src/app/property/property.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { CategoryService } from '../category.service';
+import { DataSharingService } from '../data-sharing.service';
+import { LocationService } from '../location.service';
+import { Property } from '../property';
+import { PropertyService } from '../property.service';
 
 @Component({
   selector: 'app-property',
@@ -6,10 +13,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property.component.css']
 })
 export class PropertyComponent implements OnInit {
+  reviews: string[]=[];
 
-  constructor() { }
+  constructor(public auth: AuthService,
+    public locationService: LocationService,
+    public categoryService: CategoryService,
+    public propertyService: PropertyService,
+    public dataSharing: DataSharingService,
+    public route: ActivatedRoute,
+    public router: Router) { }
 
   ngOnInit(): void {
+    this.propertyService.property = new Property();
+    this.setProperty();
+  }
+
+  setProperty() {
+    const propertyId = Number(this.route.snapshot.paramMap.get('propertyId'));
+    console.log("All Properties");
+    this.propertyService.getPropertyById(propertyId).subscribe((data: Property) => {this.propertyService.property = data});
+  }
+
+  userReview(reviewForm: any){
+    
   }
 
 }
